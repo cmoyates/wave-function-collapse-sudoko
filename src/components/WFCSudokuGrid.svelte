cb906aa579134848b1113bb6d3513167f1d53b1c
cmoyates <cmoyates@gmail.com>
2024-03-30 12:49:17 -0230

    Better logic and backtracking

---
+++ src/components/WFCSudokuGrid.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { WFCGridCell } from '$lib/wfc';
+	import WfcSudokuCell from './WFCSudokuCell.svelte';
+
+	export let workingGrid: WFCGridCell[][];
+</script>
+
+<div class="grid-cols-9 border-b-2 border-r-2 border-black">
+	{#each workingGrid as row}
+		<div class="grid grid-cols-9">
+			{#each row as cell}
+				<WfcSudokuCell {cell} />
+			{/each}
+		</div>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,82 +1,206 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
-	import WfcSudokuCell from '../components/WFCSudokuCell.svelte';
 	import type { WFCGridCell } from '$lib/wfc';
-	import { isSolvable } from '$lib/backtracking';
-	import InputSudokuCell from '../components/InputSudokuCell.svelte';
+	import WfcSudokuGrid from '../components/WFCSudokuGrid.svelte';
 
-	let userDefinedGrid: number[][] = [];
+	const timeStep = 10;
+
 	let baseGrid: WFCGridCell[][] = [];
-	let grid: WFCGridCell[][] = [];
+	let workingGrid: WFCGridCell[][] = [];
 
-	let interval: number;
+	// Init working grid
+	for (let y = 0; y < 9; y++) {
+		let row: WFCGridCell[] = [];
+		for (let x = 0; x < 9; x++) {
+			row.push({
+				x,
+				y,
+				value: null,
+				possibleValues: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+			});
+		}
+		workingGrid.push(row);
+	}
+	// Init base grid
+	for (let y = 0; y < 9; y++) {
+		let row: WFCGridCell[] = [];
+		for (let x = 0; x < 9; x++) {
+			row.push({
+				x,
+				y,
+				value: null,
+				possibleValues: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+			});
+		}
+		baseGrid.push(row);
+	}
 
-	let editing = false;
+	const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-	const numberInSame3x3 = (
-		cell1X: number,
-		cell1Y: number,
-		cell2X: number,
-		cell2Y: number
-	): boolean => {
-		const cell13x3X = Math.floor(cell1X / 3);
-		const cell13x3Y = Math.floor(cell1Y / 3);
-
-		const cell23x3X = Math.floor(cell2X / 3);
-		const cell23x3Y = Math.floor(cell2Y / 3);
-
-		return cell13x3X === cell23x3X && cell13x3Y === cell23x3Y;
-	};
-
-	const solve = () => {
-		interval = setInterval(() => {
-			const { cells, lowestEntropy } = getCellsWithLowestEntropy(grid);
-
-			if (cells.length === 0) {
-				console.log('Solved!');
-				clearInterval(interval);
-				return;
-			}
-
-			if (lowestEntropy === 0) {
-				console.log('Retrying...');
-				// resetToUserDefined();
-				solve();
-				return;
-			}
-
-			const cell = cells[Math.floor(Math.random() * cells.length)];
-
-			const value = cell.possibleValues[Math.floor(Math.random() * cell.possibleValues.length)];
-
-			setValue(cell, value);
-		}, 0);
-	};
-
-	const setValue = (cell: WFCGridCell, value: number) => {
-		cell.value = value;
-
-		for (let y = 0; y < grid.length; y++) {
-			for (let x = 0; x < grid[y].length; x++) {
-				const otherCell = grid[y][x];
-
-				if (otherCell.value !== null) continue;
-
-				if (
-					otherCell.x === cell.x ||
-					otherCell.y === cell.y ||
-					numberInSame3x3(cell.x, cell.y, otherCell.x, otherCell.y)
-				) {
-					otherCell.possibleValues = otherCell.possibleValues.filter((v) => v !== value);
+	const recalculateCellPossibleValues = (cell: WFCGridCell) => {
+		for (let y = 0; y < workingGrid.length; y++) {
+			for (let x = 0; x < workingGrid[y].length; x++) {
+				const cell = workingGrid[y][x];
+				if (cell.value !== null) {
+					cell.possibleValues = [];
+					continue;
 				}
 
-				grid[y][x] = otherCell;
+				cell.possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+				// Check row
+				for (let i = 0; i < 9; i++) {
+					cell.possibleValues = cell.possibleValues.filter(
+						(possibleValue) => possibleValue !== workingGrid[y][i].value
+					);
+				}
+
+				// Check column
+				for (let i = 0; i < 9; i++) {
+					cell.possibleValues = cell.possibleValues.filter(
+						(possibleValue) => possibleValue !== workingGrid[i][x].value
+					);
+				}
+
+				// Check 3x3
+				const cell3x3X = Math.floor(x / 3);
+				const cell3x3Y = Math.floor(y / 3);
+				for (let y = 0; y < 3; y++) {
+					for (let x = 0; x < 3; x++) {
+						cell.possibleValues = cell.possibleValues.filter(
+							(possibleValue) =>
+								possibleValue !== workingGrid[y + cell3x3Y * 3][x + cell3x3X * 3].value
+						);
+					}
+				}
+			}
+		}
+	};
+
+	const recalculateConnectedCellPossibleValues = (cell: WFCGridCell) => {
+		// Row
+		for (let i = 0; i < 9; i++) {
+			const connectedCell = workingGrid[cell.y][i];
+			if (connectedCell.value === null) {
+				recalculateCellPossibleValues(connectedCell);
 			}
 		}
 
-		grid[cell.y][cell.x] = cell;
+		// Column
+		for (let i = 0; i < 9; i++) {
+			const connectedCell = workingGrid[i][cell.x];
+			if (connectedCell.value === null) {
+				recalculateCellPossibleValues(connectedCell);
+			}
+		}
 
-		console.log('Set value', value, 'at', cell.x, cell.y);
+		// 3x3
+		const cell3x3X = Math.floor(cell.x / 3);
+		const cell3x3Y = Math.floor(cell.y / 3);
+		for (let y = 0; y < 3; y++) {
+			for (let x = 0; x < 3; x++) {
+				const connectedCell = workingGrid[y + cell3x3Y * 3][x + cell3x3X * 3];
+				if (connectedCell.value === null) {
+					recalculateCellPossibleValues(connectedCell);
+				}
+			}
+		}
+	};
+
+	const startSolve = async () => {
+		await solveStep();
+		console.log('Complete');
+		let solved = isSolved(workingGrid);
+		if (solved) {
+			console.log('The puzzle has been solved!');
+		} else {
+			console.log('This puzzle has no solution.');
+		}
+	};
+
+	const isMoveValid = (grid: WFCGridCell[][], x: number, y: number, value: number): boolean => {
+		// Check row
+		for (let i = 0; i < 9; i++) {
+			if (grid[y][i].value === value) {
+				return false;
+			}
+		}
+
+		// Check column
+		for (let i = 0; i < 9; i++) {
+			if (grid[i][x].value === value) {
+				return false;
+			}
+		}
+
+		// Check 3x3
+		const cell3x3X = Math.floor(x / 3);
+		const cell3x3Y = Math.floor(y / 3);
+		for (let y = 0; y < 3; y++) {
+			for (let x = 0; x < 3; x++) {
+				if (grid[y + cell3x3Y * 3][x + cell3x3X * 3].value === value) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	};
+
+	const solveStep = async (): Promise<boolean> => {
+		const { cells, lowestEntropy } = getCellsWithLowestEntropy(workingGrid);
+
+		if (cells.length === 0) {
+			return true;
+		}
+
+		if (lowestEntropy === 0) {
+			return false;
+		}
+
+		const cell = cells[Math.floor(Math.random() * cells.length)];
+
+		let possibleValues = cell.possibleValues.slice();
+
+		while (cell.possibleValues.length !== 0) {
+			const value = cell.possibleValues[Math.floor(Math.random() * cell.possibleValues.length)];
+
+			console.log(`Trying ${value} at ${cell.x}, ${cell.y}`);
+
+			if (!isMoveValid(workingGrid, cell.x, cell.y, value)) {
+				workingGrid[cell.y][cell.x].possibleValues = workingGrid[cell.y][
+					cell.x
+				].possibleValues.filter((possibleValue) => possibleValue !== value);
+
+				continue;
+			}
+
+			cell.value = value;
+			workingGrid[cell.y][cell.x] = cell;
+			recalculateConnectedCellPossibleValues(cell);
+
+			await sleep(timeStep);
+
+			let solveStepResult = await solveStep();
+
+			if (solveStepResult) {
+				cell.possibleValues = [];
+				return true;
+			}
+
+			await sleep(timeStep);
+
+			console.log(`Removing ${value} at ${cell.x}, ${cell.y}`);
+
+			possibleValues = possibleValues.filter((possibleValue) => possibleValue !== value);
+			cell.possibleValues = possibleValues.slice();
+
+			cell.value = null;
+
+			workingGrid[cell.y][cell.x] = cell;
+		}
+
+		recalculateConnectedCellPossibleValues(cell);
+		return false;
 	};
 
 	const getCellsWithLowestEntropy = (grid: WFCGridCell[][]) => {
@@ -101,52 +225,35 @@
 		return { cells, lowestEntropy };
 	};
 
-	// const resetToUserDefined = () => {
-	// 	clearInterval(interval);
-
-	// 	for (let y = 0; y < grid.length; y++) {
-	// 		for (let x = 0; x < grid[y].length; x++) {
-	// 			grid[y][x].value = userDefinedGrid[y][x].value;
-	// 			grid[y][x].possibleValues = [...userDefinedGrid[y][x].possibleValues];
-	// 		}
-	// 	}
-	// };
-
-	const resetGrid = (grid: WFCGridCell[][]) => {
-		clearInterval(interval);
-
-		for (let y = 0; y < 9; y++) {
-			let row: WFCGridCell[] = [];
-			for (let x = 0; x < 9; x++) {
-				row.push({
-					x,
-					y,
-					value: null,
-					possibleValues: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-				});
+	const resetWorkingGridToBaseGrid = () => {
+		for (let y = 0; y < workingGrid.length; y++) {
+			for (let x = 0; x < workingGrid[y].length; x++) {
+				workingGrid[y][x].value = baseGrid[y][x].value;
+				workingGrid[y][x].possibleValues = [...baseGrid[y][x].possibleValues];
 			}
-			grid.push(row);
 		}
 	};
 
-	const resetUserDefined = () => {
-		userDefinedGrid = [];
-		for (let y = 0; y < 9; y++) {
-			let row: number[] = [];
-			for (let x = 0; x < 9; x++) {
-				row.push(0);
+	const resetBaseGrid = () => {
+		for (let y = 0; y < workingGrid.length; y++) {
+			for (let x = 0; x < workingGrid[y].length; x++) {
+				baseGrid[y][x].value = null;
+				baseGrid[y][x].possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 			}
-			userDefinedGrid.push(row);
 		}
 	};
 
-	onDestroy(() => {
-		clearInterval(interval);
-	});
+	const isSolved = (grid: WFCGridCell[][]): boolean => {
+		for (let y = 0; y < grid.length; y++) {
+			for (let x = 0; x < grid[y].length; x++) {
+				if (grid[y][x].value === null) {
+					return false;
+				}
+			}
+		}
 
-	resetGrid(grid);
-	resetUserDefined();
-	// resetGrid(userDefinedGrid);
+		return true;
+	};
 </script>
 
 <div class="flex min-h-screen w-full flex-col items-center justify-center space-y-8">
@@ -154,67 +261,19 @@
 		<p class="text-4xl font-thin">Wave Function Collapse</p>
 		<p class="text-6xl font-thin">Sudoku</p>
 	</div>
-	<div class="grid-cols-9 border-b-2 border-r-2 border-black">
-		{#each grid as row, y}
-			<div class="grid grid-cols-9">
-				{#each row as cell, x}
-					{#if !editing}
-						<WfcSudokuCell {cell} />
-					{:else}
-						<InputSudokuCell grid={userDefinedGrid} {x} {y} />
-					{/if}
-				{/each}
-			</div>
-		{/each}
-	</div>
+	<WfcSudokuGrid {workingGrid} />
 	<div class="flex flex-row items-center justify-center space-x-4">
-		{#if !editing}
-			<button
-				class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
-				on:click={solve}
-			>
-				Solve
-			</button>
-			<button
-				class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
-				on:click={() => {}}
-			>
-				Reset
-			</button>
-			<button
-				class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
-				on:click={() => {
-					editing = !editing;
-				}}
-			>
-				Edit
-			</button>
-		{:else}
-			<button
-				class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
-				on:click={() => {
-					console.log('Confirm');
-					console.log('Is this solvable?:', isSolvable(userDefinedGrid));
-				}}
-			>
-				Confirm
-			</button>
-			<button
-				class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
-				on:click={() => {
-					console.log('Cancel');
-				}}
-			>
-				Cancel
-			</button>
-			<button
-				class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
-				on:click={() => {
-					console.log('Undo');
-				}}
-			>
-				Undo
-			</button>
-		{/if}
+		<button
+			class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
+			on:click={startSolve}
+		>
+			Solve
+		</button>
+		<button
+			class="rounded border border-black px-4 py-1 font-thin hover:bg-black hover:font-extralight hover:text-white"
+			on:click={resetWorkingGridToBaseGrid}
+		>
+			Reset
+		</button>
 	</div>
 </div>
